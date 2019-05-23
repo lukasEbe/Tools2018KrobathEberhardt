fd856408730e19f60e0e725deb559e1028249b73
Nikolaus Krobath <n.krobath@gmail.com>
2019-05-23 14:51:04 +0200

    Bug fixed

--- Krobath_Eberhardt_android/Android.cpp
+++ Krobath_Eberhardt_android/Android.cpp
@@ -5,10 +5,14 @@ using namespace std;
 
  int main(){
     cout << "Hi";
-	cout << "A different line of code"
+     cout << "A different line of code";
 }
 
  void thisIsANewMethod() {
-     cout << "Hier steht ein Code."
-	 cout << "A different line of code"
+     cout << "Hier steht ein Code.";
+     cout << "A different line of code";
+     bool f = false;
+     if(f){
+         cout << "Bug is fixed";
+     }
  }
